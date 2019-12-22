0ac1cb69d5177b33f77532d4d52d0803a98fc34b
Qui <lquiitfuture@gmail.com>
2019-12-22 22:30:46 +0700

    add proect asyn main

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Async_Main
+{
+    class Program
+    {
+        //before C# 7.1
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before C# 7.1, To use async method");
+            Console.WriteLine($"Main Method execution started at {System.DateTime.Now}");
+            Task.Delay(2000).GetAwaiter().GetResult();
+            Console.WriteLine($"Main Method execution ended at {System.DateTime.Now}");
+            Console.WriteLine("Press any key to exist.");
+            Console.ReadKey();
+        }
+
+    }
+}
